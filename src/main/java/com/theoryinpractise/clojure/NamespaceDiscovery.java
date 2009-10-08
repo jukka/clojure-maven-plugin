jukka/clojure-maven-plugin

commit a4f34e1fbfc66a3f665dac0f0025d9d045c26c3e

Reformat a long one-liner to a sequence of steps to simplify the code (and potential diffs).

src/main/java/com/theoryinpractise/clojure/NamespaceDiscovery.java
@@ -113,8 +113,10 @@ public class NamespaceDiscovery {
                 Matcher matcher = nsPattern.matcher(line);
 
                 if (matcher.find()) {
-                    String ns = file.getPath().substring(path.getPath().length() + 1, file.getPath().length() - 4).replaceAll("/", ".").replaceAll("_", "-");
-
+                    String ns = file.getPath();
+                    ns = ns.substring(path.getPath().length() + 1, ns.length() - 4);
+                    ns = ns.replaceAll("/", ".");
+                    ns = ns.replaceAll("_", "-");
 
                     log.debug("Found namespace " + ns + " in file " + file.getPath());
                     namespaces.add(ns);

commit cb2dc8918b23d7a77e368a1dcae1de673e60bd14

Use File.separatorChar instead of "/" to avoid problems on Windows.
There was some problem in how my earlier related changes got merged to
talios' master branch. The change b90066a0 got applied also in commit
a4f34e1f, which caused the merge conflict a20e191e and the apparently
accidental reverting of my subsequent changes in commit 8d76d45c.

This commit reintroduces the lost changes:

Change 265a6cf7: Make it clear that we are removing ".clj" from the
end of the path when creating a namespace string.

Change 5381ef37: Use String.replace(char, char) instead of
String.replaceAll(String, String) for single-character replacements.
This is faster and avoids potential regexp quoting issues.

Change 202ce7a9: Fix path-based namespace detection on Windows platforms.

Only the last change affects the behaviour of the code, fixing the namespace
detection problem on Windows. No new test case is included with these changes,
as the problem was already causing the existing NamespaceDiscoveryTest to fail
on Windows.

src/main/java/com/theoryinpractise/clojure/NamespaceDiscovery.java
@@ -114,9 +114,11 @@ public class NamespaceDiscovery {
 
                 if (matcher.find()) {
                     String ns = file.getPath();
-                    ns = ns.substring(path.getPath().length() + 1, ns.length() - 4);
-                    ns = ns.replaceAll("/", ".");
-                    ns = ns.replaceAll("_", "-");
+                    ns = ns.substring(
+                            path.getPath().length() + 1,
+                            ns.length() - ".clj".length());
+                    ns = ns.replace(File.separatorChar, '.');
+                    ns = ns.replace('_', '-');
 
                     log.debug("Found namespace " + ns + " in file " + file.getPath());
                     namespaces.add(ns);